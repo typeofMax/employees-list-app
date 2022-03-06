4184ad64909150cac0c34926a2f3215d34dbf05b
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-03-06 23:43:49 +0300

    Now list items created from static data

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,15 +1,21 @@
-import EmployeesListItem from "../employees-list-item/employees-list-item";
+import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({ data }) => {
+    const listItem = data.map((item) => {
+        const {id, ...props} = item;
+
+        return (
+            <EmployeesListItem key={id} {...props} />
+        );
+    });
+
     return (
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+        <ul className='app-list list-group'>
+            {listItem}
         </ul>
-    )
-}
+    );
+};
 
 export default EmployeesList;
