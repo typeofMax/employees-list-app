624b40781ef8234842781819ac6b0324ece05453
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-03-06 23:46:06 +0300

    Added the ability to mark an employee for promotion or mark star

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,24 +1,64 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            stared: false,
+        };
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onIncrease = () => {
+        this.setState(({ increase }) => ({
+            increase: !increase,
+        }));
+    };
+
+    onStared = () => {
+        this.setState(({ stared }) => ({
+            stared: !stared,
+        }));
+    };
+
+    render() {
+        const { increase, stared } = this.state;
+
+        let classes = 'list-group-item d-flex justify-content-between';
+
+        if (increase) {
+            classes += ' increase';
+        }
+        if (stared) {
+            classes += ' like';
+        }
+
+        return (
+            <li className={classes}>
+                <span className='list-group-item-label' onClick={this.onStared}>{this.props.name}</span>
+                <input
+                    type='text'
+                    className='list-group-item-input'
+                    defaultValue={this.props.salary + '$'}
+                />
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button
+                        type='button'
+                        className='btn-cookie btn-sm '
+                        onClick={this.onIncrease}
+                    >
+                        <i className='fas fa-cookie'></i>
+                    </button>
+
+                    <button type='button' className='btn-trash btn-sm '>
+                        <i className='fas fa-trash'></i>
+                    </button>
+                    <i className='fas fa-star'></i>
+                </div>
+            </li>
+        );
+    }
 }
 
 export default EmployeesListItem;
