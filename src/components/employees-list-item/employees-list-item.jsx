f8ae67a7415337d3c55dd1089f48e17689268c13
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-03-15 21:01:21 +0300

    changed component to functional and made state lifting to <App> component

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,67 +1,52 @@
-import { Component } from 'react';
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            stared: false,
-        };
+const EmployeesListItem = (props) => {
+    const { name, salary, increase, rise, onDelete, onToggleProp } = props;
+
+    let classes = 'list-group-item d-flex justify-content-between';
+
+    if (increase) {
+        classes += ' increase';
+    }
+    if (rise) {
+        classes += ' like';
     }
 
-    onIncrease = () => {
-        this.setState(({ increase }) => ({
-            increase: !increase,
-        }));
-    };
+    return (
+        <li className={classes}>
+            <span
+                className='list-group-item-label'
+                data-toggle='rise'
+                onClick={onToggleProp}
+            >
+                {name}
+            </span>
+            <input
+                type='text'
+                className='list-group-item-input'
+                defaultValue={salary + '$'}
+            />
+            <div className='d-flex justify-content-center align-items-center'>
+                <button
+                    type='button'
+                    className='btn-cookie btn-sm '
+                    data-toggle='increase'
+                    onClick={onToggleProp}
+                >
+                    <i className='fas fa-cookie'></i>
+                </button>
 
-    onStared = () => {
-        this.setState(({ stared }) => ({
-            stared: !stared,
-        }));
-    };
-
-    render() {
-        const { name, salary, onDelete } = this.props;
-        const { increase, stared } = this.state;
-
-        let classes = 'list-group-item d-flex justify-content-between';
-
-        if (increase) {
-            classes += ' increase';
-        }
-        if (stared) {
-            classes += ' like';
-        }
-
-        return (
-            <li className={classes}>
-                <span className='list-group-item-label' onClick={this.onStared}>
-                    {name}
-                </span>
-                <input
-                    type='text'
-                    className='list-group-item-input'
-                    defaultValue={salary + '$'}
-                />
-                <div className='d-flex justify-content-center align-items-center'>
-                    <button
-                        type='button'
-                        className='btn-cookie btn-sm '
-                        onClick={this.onIncrease}
-                    >
-                        <i className='fas fa-cookie'></i>
-                    </button>
-
-                    <button type='button' className='btn-trash btn-sm ' onClick={onDelete}>
-                        <i className='fas fa-trash'></i>
-                    </button>
-                    <i className='fas fa-star'></i>
-                </div>
-            </li>
-        );
-    }
-}
+                <button
+                    type='button'
+                    className='btn-trash btn-sm '
+                    onClick={onDelete}
+                >
+                    <i className='fas fa-trash'></i>
+                </button>
+                <i className='fas fa-star'></i>
+            </div>
+        </li>
+    );
+};
 
 export default EmployeesListItem;
