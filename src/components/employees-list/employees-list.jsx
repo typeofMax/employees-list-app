f23dce00ba1569ee008dcd239198b9ee5fcbdea5
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-07-19 20:17:57 +0300

    Create empty list message

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -3,26 +3,32 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
 const EmployeesList = ({ data, onDelete, onToggleProp, changeSalary }) => {
-    const listItem = data.map((item) => {
-        const { id, ...props } = item;
+	if (!data.length) {
+		return (
+			<ul className='app-list list-group'>
+				<li className='list-group-item d-flex justify-content-between'>
+					Сотрудники не найдены
+				</li>
+			</ul>
+		);
+	}
+	const listItem = data.map((item) => {
+		const { id, ...props } = item;
 
-        return (
-            <EmployeesListItem
-                key={id}
-                {...props}
-                onDelete={() => onDelete(id)}
-                onToggleProp={(e) =>
-                    onToggleProp(
-                        id,
-                        e.currentTarget.getAttribute('data-toggle')
-                    )
-                }
-                changeSalary={changeSalary}
-            />
-        );
-    });
+		return (
+			<EmployeesListItem
+				key={id}
+				{...props}
+				onDelete={() => onDelete(id)}
+				onToggleProp={(e) =>
+					onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))
+				}
+				changeSalary={changeSalary}
+			/>
+		);
+	});
 
-    return <ul className='app-list list-group'>{listItem}</ul>;
+	return <ul className='app-list list-group'>{listItem}</ul>;
 };
 
 export default EmployeesList;
