1133da388b8d70057b6a938d41e6683bc1006886
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-07-20 18:25:21 +0300

    feat: Create editable company name

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,15 +1,42 @@
+//@Libs
+import { Component } from 'react';
+import cn from 'classnames';
+//@Styles
 import './app-info.css';
+//@Images
+import write from '../../assets/icons/writing.png';
 
-const AppInfo = (props) => {
-	return (
-		<div className='app-info'>
-			<h1 className='app-title'>Учет сотрудников в компании N</h1>
-			<h2 className='app-subtitle'>
-				Общее число сотрудников: {props.employeesCount}
-			</h2>
-			<h2 className='app-subtitle'>Премию получат: {props.increased}</h2>
-		</div>
-	);
-};
+class AppInfo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { companyName: '' };
+	}
+
+	changeCompanyName = (name) => {
+		this.setState(() => ({ companyName: name }));
+	};
+
+	render() {
+		return (
+			<div className='app-info'>
+				<div className='app-title'>
+					<h1 className='app-title__text'>Учет сотрудников в компании </h1>
+					<input
+						className={cn('app-title__company',{'nofill-pulse': !this.state.companyName})}
+						type='text'
+						placeholder='Введите название...'
+						value={this.state.companyName}
+						onChange={(e) => this.changeCompanyName(e.target.value)}
+					/>
+					{this.state.companyName ? null : <img src={write} alt='' />}
+				</div>
+				<h2 className='app-subtitle'>
+					Общее число сотрудников: {this.props.employeesCount}
+				</h2>
+				<h2 className='app-subtitle'>Премию получат: {this.props.increased}</h2>
+			</div>
+		);
+	}
+}
 
 export default AppInfo;
